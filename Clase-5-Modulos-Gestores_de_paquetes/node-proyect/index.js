78e439b54077799d1bf6ade5066ad0900158b7bd
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2026-04-12 21:27:19 -0300

    Instalación de dependencias y creación de scripts

---
+++ Clase-5-Modulos-Gestores_de_paquetes/node-proyect/index.js
@@ -0,0 +1,5 @@
+console.log("==============================================");
+console.log("Hola, soy un mensaje de prueba en node.js");
+console.log("Instalación de dependencias [nodemon] exitoso");
+console.log("Ejecución de script [start] exitoso");
+console.log("==============================================");
